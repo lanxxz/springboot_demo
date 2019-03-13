f77de1b2984dc2fdfb49cf32becd25ee0d3348f1
lanxxz <15080165456@163.com>
2019-03-14 00:37:58 +0800

    补充修改

--- persistence/src/main/java/com/alien/practice/springboot_demo/repository/MyFirstLeaveRepository.java
+++ persistence/src/main/java/com/alien/practice/springboot_demo/repository/MyFirstLeaveRepository.java
@@ -1,8 +1,11 @@
 package com.alien.practice.springboot_demo.repository;
 
 import com.alien.practice.springboot_demo.annotation.FirstLeaveRepository;
+import com.alien.practice.springboot_demo.annotation.SecondLeaveRepository;
 
-@FirstLeaveRepository(value = "myFirstLeaveRepository")
+//@FirstLeaveRepository(value = "myFirstLeaveRepository")
+
+@SecondLeaveRepository(value = "myFirstLeaveRepository")
 public class MyFirstLeaveRepository {
 
 }
