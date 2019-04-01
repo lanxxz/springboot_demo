6255853529958135329c5bb5b1d79e1eea334d61
lanxxz <15080165456@163.com>
2019-04-02 00:21:26 +0800

    添加注释

--- spring-application/src/main/java/com/alien/practice/springbootdemo/context/HelloWorldApplicationContextInitializer.java
+++ spring-application/src/main/java/com/alien/practice/springbootdemo/context/HelloWorldApplicationContextInitializer.java
@@ -6,6 +6,10 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 
 /**
+ * {@link ApplicationContextInitializer} 实现类 <br/>
+ * 参考这两个实现：<br/>
+ * org.springframework.boot.autoconfigure.SharedMetadataReaderFactoryContextInitializer<br/>
+ * org.springframework.boot.autoconfigure.logging.ConditionEvaluationReportLoggingListener
  * @author: Alien
  * @since: 2019/4/1 23:41
  */
