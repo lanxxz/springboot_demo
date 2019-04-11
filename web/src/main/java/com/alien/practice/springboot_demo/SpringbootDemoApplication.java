0b53e8e47561ab977fb029a0581ed0fe6b0b1ce2
lanxxz <15080165456@163.com>
2019-04-11 22:00:43 +0800

    删除多余代码

--- web/src/main/java/com/alien/practice/springboot_demo/SpringbootDemoApplication.java
+++ web/src/main/java/com/alien/practice/springboot_demo/SpringbootDemoApplication.java
@@ -1,23 +1,10 @@
 package com.alien.practice.springboot_demo;
 
-import com.alien.practice.springboot_demo.common.BeanTool;
-import com.alien.practice.springboot_demo.common.ClassScaner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootDemoApplication {
-
-    @Bean
-    public BeanTool getBeanTool() {
-        return new BeanTool();
-    }
-
-    @Bean
-    public ClassScaner getClassScaner() {
-        return new ClassScaner();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDemoApplication.class, args);
