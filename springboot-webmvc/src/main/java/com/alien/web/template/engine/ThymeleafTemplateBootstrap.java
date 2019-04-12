6d32bf2ca8779063b12947f0abc9e4eb88c9ac8f
lanxxz <15080165456@163.com>
2019-04-12 23:04:57 +0800

    Thymeleaf 和 Spring 资源 整合简单示例

--- springboot-webmvc/src/main/java/com/alien/web/template/engine/ThymeleafTemplateBootstrap.java
+++ springboot-webmvc/src/main/java/com/alien/web/template/engine/ThymeleafTemplateBootstrap.java
@@ -1,7 +1,16 @@
 package com.alien.web.template.engine;
 
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring5.SpringTemplateEngine;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 /**
  * Thymeleaf 模板引擎 引导类
@@ -11,14 +20,42 @@ import org.thymeleaf.spring5.SpringTemplateEngine;
  */
 public class ThymeleafTemplateBootstrap {
 
-    public static void main(String[] args) {
+//    public static void main(String[] args) {
+//        //构建引擎
+//        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+//        //创建渲染上下文
+//        Context context = new Context();
+//        context.setVariable("message", "Hi, Alien!");
+//        //模板内容
+//        String content = "<p th:text=\"${message}\">!!!</p>";
+//        //渲染处理结果
+//        String result = templateEngine.process(content, context);
+//        //输出渲染处理结果
+//        System.out.println(result);
+//    }
+
+    public static void main(String[] args) throws IOException {
         //构建引擎
         SpringTemplateEngine templateEngine = new SpringTemplateEngine();
         //创建渲染上下文
         Context context = new Context();
         context.setVariable("message", "Hi, Alien!");
+
+        //读取内容 从 classpath:/templates/thymeleaf/hello-world.html
+        //ResourceLoader
+        ResourceLoader resourceLoader = new DefaultResourceLoader();
+        //通过 classpath 获取 Resource
+        Resource resource = resourceLoader.getResource("classpath:/templates/thymeleaf/hello-world.html");
+        File templateFile = resource.getFile();
+
+        //文件输入输出流
+        FileInputStream inputStream = new FileInputStream(templateFile);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        //复制 流 的内容
+        IOUtils.copy(inputStream, outputStream);
+
         //模板内容
-        String content = "<p th:text=\"${message}\">!!!</p>";
+        String content = outputStream.toString("UTF-8");
         //渲染处理结果
         String result = templateEngine.process(content, context);
         //输出渲染处理结果
