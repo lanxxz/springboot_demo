6132bdc03e1b8fe0134bc75feac42ad84eb9afc5
lanxxz <15080165456@163.com>
2019-05-08 00:15:20 +0800

    Servlet 异步支持

---
+++ spring-servlet/src/main/java/com/alien/practice/web/servlet/controller/HelloWorldAsyncController.java
@@ -0,0 +1,29 @@
+package com.alien.practice.web.servlet.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
+
+/**
+ * Hello World 异步 {@link RestController}
+ *
+ * @author Alien
+ * @since 2019/5/7 23:35
+ */
+@RestController
+public class HelloWorldAsyncController {
+
+    @GetMapping("/hello-world")
+    public DeferredResult<String> helloWorld() {
+        DeferredResult<String> result = new DeferredResult<String>();
+
+        result.setResult("Hello, My World!");
+        //完成时
+        result.onCompletion(() -> {
+            System.out.println("HelloWorldAsyncController helloWorld() 执行结束！");
+        });
+
+        return result;
+    }
+
+}
---
+++ spring-servlet/src/main/java/com/alien/practice/web/servlet/support/DefaultAnnotationConfigDispatcherServletInitializer.java
@@ -0,0 +1,32 @@
+package com.alien.practice.web.servlet.support;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+/**
+ * Spring Web MVC 自动装配 默认实现
+ *
+ * @author Alien
+ * @since 2019/5/7 23:18
+ */
+//@Configuration
+@ComponentScan(basePackages = "com.alien.practice.web.servlet.controller")
+public class DefaultAnnotationConfigDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0]; //相当于 web.xml
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() { //DispatcherServlet
+        return new Class[] {
+                getClass() //返回当前类
+        };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"}; //Servlet Mapping 映射
+    }
+}
