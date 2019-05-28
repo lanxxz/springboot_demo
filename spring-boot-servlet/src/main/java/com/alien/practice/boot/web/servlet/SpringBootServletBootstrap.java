b8ea0f465810318548a586d059751caf30216306
lanxxz <15080165456@163.com>
2019-05-28 22:46:58 +0800

    ServletComponentScan 加载 web Servlet

--- spring-boot-servlet/src/main/java/com/alien/practice/boot/web/servlet/SpringBootServletBootstrap.java
+++ spring-boot-servlet/src/main/java/com/alien/practice/boot/web/servlet/SpringBootServletBootstrap.java
@@ -2,6 +2,7 @@ package com.alien.practice.boot.web.servlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 /**
  * Spring boot servlet 引导类
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
  * @since 2019/5/23 23:33
  */
 @EnableAutoConfiguration
+@ServletComponentScan(basePackages = "com.alien.practice.web.servlet")
 public class SpringBootServletBootstrap {
 
     public static void main(String[] args) {
