d56a107a30d0c9868eaf3d92259e872de62821d7
lanxxz <15080165456@163.com>
2019-05-28 23:05:30 +0800

    ServletContextInitiializer

--- spring-boot-servlet/src/main/java/com/alien/practice/boot/web/servlet/SpringBootServletBootstrap.java
+++ spring-boot-servlet/src/main/java/com/alien/practice/boot/web/servlet/SpringBootServletBootstrap.java
@@ -3,6 +3,13 @@ package com.alien.practice.boot.web.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import javax.servlet.DispatcherType;
+import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
 
 /**
  * Spring boot servlet 引导类
@@ -18,4 +25,14 @@ public class SpringBootServletBootstrap {
         SpringApplication.run(SpringBootServletBootstrap.class, args);
     }
 
+    @Bean
+    public ServletContextInitializer servletContextInitializer() {
+        return servletContext -> {
+            CharacterEncodingFilter filter = new CharacterEncodingFilter();
+            FilterRegistration.Dynamic registration = servletContext.addFilter("filter", filter);
+            registration.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), false, "/");
+
+
+        };
+    }
 }
