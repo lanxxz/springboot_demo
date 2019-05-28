4c424453040656e26a018cd413939cf811de408a
lanxxz <15080165456@163.com>
2019-05-28 23:23:21 +0800

    ServletRegistrationBean

--- spring-boot-servlet/src/main/java/com/alien/practice/boot/web/servlet/SpringBootServletBootstrap.java
+++ spring-boot-servlet/src/main/java/com/alien/practice/boot/web/servlet/SpringBootServletBootstrap.java
@@ -1,10 +1,14 @@
 package com.alien.practice.boot.web.servlet;
 
+import com.alien.practice.web.servlet.AsyncServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
 import javax.servlet.DispatcherType;
@@ -18,11 +22,23 @@ import java.util.EnumSet;
  * @since 2019/5/23 23:33
  */
 @EnableAutoConfiguration
-@ServletComponentScan(basePackages = "com.alien.practice.web.servlet")
+//@ServletComponentScan(basePackages = "com.alien.practice.web.servlet")
 public class SpringBootServletBootstrap {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootServletBootstrap.class, args);
+    }
+
+//    使用该方法很难改造，会和默认的DispatcherServlet产生冲突
+//    @Bean
+//    @Order(Ordered.HIGHEST_PRECEDENCE)
+//    public AsyncServlet asyncServlet() {
+//        return new AsyncServlet();
+//    }
+
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean() {
+        return new ServletRegistrationBean(new AsyncServlet(), "/async-servlet");
     }
 
     @Bean
@@ -31,8 +47,6 @@ public class SpringBootServletBootstrap {
             CharacterEncodingFilter filter = new CharacterEncodingFilter();
             FilterRegistration.Dynamic registration = servletContext.addFilter("filter", filter);
             registration.addMappingForUrlPatterns(EnumSet.of(DispatcherType.REQUEST), false, "/");
-
-
         };
     }
 }
